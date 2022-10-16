0306cf3850f76801b3cd21f7cccd1ac77e221a8b
Graham Theobald <Graham_Theobald@live.co.uk>
2022-10-16 12:11:47 +0100

    mobile view

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'BaiJamjuree': ['Bai Jamjuree', "sans-serif"]
+      },
+      colors: {
+        StrongCyan: "hsl(171, 66%, 44%)",
+        LightBlue: "hsl(233, 100%, 69%)",
+        DarkGrayishBlue: "hsl(210, 10%, 33%)",
+        GrayishBlue: "hsl(201, 11%, 66%)",
+      }
+    },
+  },
+  plugins: [],
+}
